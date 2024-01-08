02a818b032c9937805f947276383d4d2059dc66c
devintarn <devin.tran06@gmail.com>
2024-01-08 00:13:33 -0500

    code

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,27 @@
-
+import java.util.*;
 public class Main {
+
+    private static void printArray(int[] array){
+        int num;
+        for(int i = 0; i < 1000; i++){
+            for(int j = 0; j < 10; j++){
+                num = array[i];
+                System.out.print("\t" + num);
+            }
+            System.out.println();
+        }
+    }
+
+    private static int[] createRandomArray(Random random){
+        int[] array = new int[100000];
+        for(int i = 0; i < array.length; i++){
+            array[i] = random.nextInt(100000) + 1;
+        }return array;
+    }
     public static void main(String[] args) {
+        Random random = new Random();
+        int test[] = createRandomArray(random);
+        printArray(test);
 
     }
 }
