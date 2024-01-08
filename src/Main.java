c9018fa0238dc5e37d61191fad390e712e9e83be
devintarn <devin.tran06@gmail.com>
2024-01-08 00:32:57 -0500

    hi

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
 import java.util.*;
 public class Main {
 
-
+    /** Method Name: createRandomArray
+     * @Author Devin Tran
+     * @Date December 20th, 2023
+     * @Modified December 20th, 2023
+     * @Description Creates an array of 100,000 elements(integers 1-100,000)
+     * @Parameters The object to randomize the integers
+     * @Returns The filled array of random integers, Data Type: int[]
+     * Dependencies: Random
+     * Throws/Exceptions: N/A
+     */
     private static int[] createRandomArray(Random random){
         int[] array = new int[100000];
         for(int i = 0; i < array.length; i++){
